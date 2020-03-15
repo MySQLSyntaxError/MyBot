ba9d9b836ec2e3ad640d61e2b964ebbcb5fab65e
MySQLSyntaxError <60144168+MySQLSyntaxError@users.noreply.github.com>
2020-03-15 20:26:56 +0100

    Create bot.py
    Update 0.0.1 | MySQLSyntaxError

---
+++ discord_bot/bot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import asyncio
+import datetime
+import sqlite3
+import json
+
+
+async def get_prefix(bot, message):
+    async with open('prefixes.json', 'r') as f:
+        data = json.load(f)
+
+    if data[str(message.guild.id)] is not None:
+        return data[str(message.guild.id)]
+    else:
+        return '!'
+
+
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True)
+bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    print("Logged in as User: " + bot.user.name)
+    while 1 == 1:
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"on {len(bot.guilds)} Servers"))
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"on {len(bot.get_all_members())} Users"))
+        await asyncio.sleep(5)
+
+
+bot.run('')
